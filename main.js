67993a2856f9fd5a59c949a1021361ad018b2c9c
Deaano <67870671+Deaano@users.noreply.github.com>
2021-03-17 14:08:05 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const textArrayData = ["A Front-End Web Developer."]
 
 const typedText = document.querySelector("#typedtext");
+const burgerButton = document.querySelector(".burger-menu");
+const menu = document.querySelector(".menu-list");
+const menuLinks = document.querySelectorAll(".menu-links");
 
 let startDelay = 1000;
 let typingDelay = 90;
@@ -8,6 +11,12 @@ let charactorIndex = 0;
 let textArrayIndex = 0;
 
 
+
+burgerButton.addEventListener('click', function () {
+        menu.classList.toggle('menu-show');
+
+        
+});
 
 
 
