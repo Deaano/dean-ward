8cc9cbc48ea0c5d5c08160b3fbac14712d3c32f4
Deaano <67870671+Deaano@users.noreply.github.com>
2021-03-19 14:32:39 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,53 @@
+
+        /* ARRAY */
 const textArrayData = ["A Front-End Web Developer."]
 
+        /* SELECTORS */
 const typedText = document.querySelector("#typedtext");
-const burgerButton = document.querySelector(".burger-menu");
-
-
-
+const burgerButton = document.querySelector(".burger-box");
+const burgerLine = document.querySelector(".burger-line");
+const burgerCross = document.querySelector(".burger-cross");
+const menu = document.querySelector("#menu-list");
 
 let startDelay = 1000;
 let typingDelay = 90;
 let charactorIndex = 0;
 let textArrayIndex = 0;
 
+        /* BURGER MENU FUNCTION */
 
-
-burgerButton.addEventListener('click', function () {
+burgerLine.addEventListener('click', function () {
+               
+        menu.classList.toggle("burger-list-show");        
         
-        let menu = document.querySelector(".menu-list");
-        
-        menu.classList.toggle("menu-show");
-       
+        burgerLine.classList.toggle("burger-cross");
 
 });
 
 window.addEventListener('click', function(event){
         
-        let menu = document.querySelector(".menu-show");
-       let burger = document.querySelector(".burger-menu");
-       
-
-        if(event.target != menu && event.target != burger && event.target.parentNode != menu ){
+ if(event.target != menu && event.target != burgerLine && event.target != burgerButton && event.target.parentNode != menu ){
                
-                menu.classList.remove("menu-show");
-        
-        } 
+        menu.classList.remove("burger-list-show");
+
+        burgerLine.classList.replace("burger-cross","burger-line");
+
+ } 
+
 });
 
-
-
-
-
+        /*  TYPING EFFECT  */
 
 setTimeout(function typing (){
          
-
-       
-
-                if(charactorIndex < textArrayData[textArrayIndex].length){
+        if(charactorIndex < textArrayData[textArrayIndex].length){
         
                 typedText.textContent += textArrayData[textArrayIndex].charAt(charactorIndex);
                 charactorIndex++;
                 
-                        setTimeout(typing,typingDelay);
-                }
-        
-        
+                setTimeout(typing,typingDelay);
+        }
+          
 },startDelay);
 
 
